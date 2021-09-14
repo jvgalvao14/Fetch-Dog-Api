3c90976c1f440c06080584c195b47a7e74d18650
Joao Vitor <jv.galvao14@gmail.com>
2021-09-14 17:40:36 -0300

    Sending api data to the frontend

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
+import cors from "cors";
 import dogRoute from "./Routes/Dog";
 
 const app = express();
 
+app.use(cors());
 app.use("/dog", dogRoute);
 
 app.listen("3000", (): void => {
