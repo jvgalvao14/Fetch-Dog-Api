6643b80c05b8acfc9f112119474d6c9e37553974
Joao Vitor <jv.galvao14@gmail.com>
2021-09-15 17:15:10 -0300

    Moving the fetchDog function to a script file.

---
+++ public/dog.js
@@ -0,0 +1,11 @@
+async function fetchDog() {
+    try {
+        const dog = await fetch("http://localhost:3000/dog/");
+        const data = await dog.json();
+        document.querySelector(
+            ".container"
+        ).innerHTML = `<img src="${data.message}" alt="DOG!" class="rounded mx-auto d-block" style="max-height: 600px">`;
+    } catch (error) {
+        console.log(error);
+    }
+}
